a1308c91234c0a98b88b39f4f6a464acfd908383
piotrstelmach <minor41@gmail.com>
2018-03-30 16:57:32 +0200

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://obscure-everglades-38245.herokuapp.com/v1/task';
+  var apiRoot = 'https://obscure-everglades-38245.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
