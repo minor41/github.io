b6d92cfb7640275fad9664afcdf6537f6daf5b17
piotrstelmach <minor41@gmail.com>
2018-04-12 22:43:01 +0200

    3rd com

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://obscure-everglades-38245.herokuapp.com/v1/task/';
+  const apiRoot = 'https://obscure-everglades-38245.herokuapp.com/v1/task';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
